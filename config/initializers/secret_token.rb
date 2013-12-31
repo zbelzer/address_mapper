69e9aee1634c1d9d4d46c052755ef6682e2e1a78
Zachary Belzer <zbelzer@gmail.com>
2013-12-31 14:10:18 -0600

    Initial commit. Blank Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneRailsMaps::Application.config.secret_token = '73ab9a7983273f8649aa02fd6a7e9e7a43d90a4d69c831cdc1600cddcab74286806589c3c4218e4f915e9a5d90c3283e0d0409a01db4042c78c9e4551db88ec6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+BackboneRailsMaps::Application.config.session_store :cookie_store, key: '_backbone_rails_maps_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# BackboneRailsMaps::Application.config.session_store :active_record_store
