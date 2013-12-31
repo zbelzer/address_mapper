b4b274245431e10d91d90f12d98bac9675655918
Zachary Belzer <zbelzer@gmail.com>
2013-12-31 17:16:28 -0600

    Add google maps sample app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter :set_api_keys
+
+  def set_api_keys
+    @google_api_key = ENV["GOOGLE_MAPS_API_KEY"]
+  end
 end
